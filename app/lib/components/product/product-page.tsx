f014317b46e5fd0fca51f627e2243685a1910969
deadlyunicorn <retroalex1008@gmail.com>
2023-06-28 22:46:56 +0300

    +similar products loading

--- app/lib/components/product/product-page.tsx
+++ app/lib/components/product/product-page.tsx
@@ -4,17 +4,17 @@ import Link from "next/link";
 import { Button } from "@/app/lib/components/button";
 import { Recommendation, Recommendation_Fallback } from "@/app/lib/components/product/recommendations";
 
-const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>(
+const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>{
 
-    <main 
-        className="
-            border-4 border-white">
-            
-            <div 
-                className="
-                    flex flex-wrap 
-                    justify-evenly 
-                    border-4 border-red-700">
+    const arr6 = [... new Array(6)];
+
+    return(
+    <>
+        <main 
+            className="
+            flex flex-wrap 
+            justify-evenly 
+            border-4 border-red-700">
             
                 <div 
                 // dangerous css?
@@ -35,16 +35,33 @@ const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>(
                         
                         
                     </Link>
-                     <div className="flex flex-col justify-between flex-wrap">
-                        <p className="my-4 min-h-[50px]  text-4xl text-white font-light">
-                            {listing.name}
+                     <div className="
+                        flex flex-col 
+                        justify-between flex-wrap">
+                        
+                        <p className="
+                            my-4 min-h-[50px]
+                            text-4xl
+                            text-slate-200 hover:text-white
+                            font-light">
+
+                                {listing.name}
                         </p>
+
                         <div>
                             <span className="
                                 text-2xl
-                                bg-gradient-to-r from-red-400 to-yellow-300 bg-clip-text text-transparent">
+                                bg-gradient-to-r 
+                                from-red-400 to-yellow-300 
+                                hover:from-red-300 hover:to-yellow-200
+                                bg-clip-text text-transparent">
+
                                 {listing.price["formatted_with_symbol"]}
-                                <span className="text-xs">&nbsp;(χωρίς ΦΠΑ)</span>
+
+                                <span className="text-xs">
+                                    &nbsp;(χωρίς ΦΠΑ)
+                                </span>
+                            
                             </span>
                             <Button link={listing.checkout_url.checkout}/>
                         </div>
@@ -55,7 +72,10 @@ const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>(
                     
                 </div>
                 
-                <div className="text-white text-xl font-light w-[300px] pt-10 ">
+                <div className="
+                    text-white 
+                    text-xl font-light 
+                    w-[300px] pt-10 ">
                 {
                     listing.description?
                         <div dangerouslySetInnerHTML={{__html:listing.description}}/>
@@ -63,24 +83,23 @@ const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>(
                         <p>No description found</p>
                 }
                 </div>
+            </main>
 
-                
-                
-            </div>
-            <div className="flex gap-x-5 mt-10 border-4 border-red-700">
-                {similar.map((item,index)=>
-                    <Recommendation_Fallback key={index}/>
-                    // <Recommendation listing={item} key={item.id}/>
+            <aside className="
+                flex gap-x-5 mt-10 
+                overflow-x-scroll 
+                border-4 border-red-700">
+
+                {similar.map((item)=>
+                    <Recommendation listing={item} key={item.id}/>
                 )}
-            </div>
 
+                <Recommendation_Fallback/>
+
+            </aside>
             
-
-
-            
-
-            
-        </main>
-)
+    </>
+    )
+}
 
 export default ProductPage;
