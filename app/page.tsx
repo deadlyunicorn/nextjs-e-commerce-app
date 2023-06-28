41027f3c4d2603eeff471e710bd4ce6be00f6b3e
deadlyunicorn <retroalex1008@gmail.com>
2023-06-28 20:59:43 +0300

    +loading component +split components into files

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,30 @@
-import Image from "next/image";
-import {items} from "@/lib/commerce"
-import Link from "next/link";
-import { Button } from "./button";
+import {items} from "@/app/lib/commerce"
+import { Suspense } from "react";
+import Store_Front, { Store_Front_Fallback } from "./lib/components/homepage/store_front";
 
 export default async function Home() {
+
   
 
   return (
     <main>
-      <div className="  ">
+      <div>
+
+     
+     
 
 
         <div className="flex gap-20 flex-wrap">
-          {items.map(
-            (item)=>(
-              
-              <div key={item.id} className="max-w-[200px] ">
 
-                <Link href={`/product/${item.permalink}`}>
-                <Image src={item.image?item.image["url"]:""} 
-                alt={'aa'}  height={200} width={200}
-                className="rounded-md h-[200px]" />
-                
-                  <p className="my-4 h-[50px]  text-xl text-white font-light">
-                    {item.name} 
-                  </p>
-                </Link>
-                <div className=" bg-gradient-to-r from-red-400 to-yellow-300 bg-clip-text text-transparent ">
-                      {item.price["formatted_with_symbol"]} 
-                      <span className="text-xs">&nbsp;(χωρίς ΦΠΑ)</span>
-                  </div>
-                <Button link={item.checkout_url.checkout}/>
-                  
-              </div>
-            )
-          )}
+          <Suspense fallback={<Store_Front_Fallback/>}>
+           
+            {items.map(
+                (item)=>(
+                  <Store_Front item={item} key={item.id}/>
+                )
+              )}
+
+          </Suspense>
         </div>
 
       </div>
