dcb4517b3569729eabfea334ccebc37358aae1fa
deadlyunicorn <retroalex1008@gmail.com>
2023-06-29 00:58:46 +0300

    +product fallback

--- app/lib/components/product/product-page.tsx
+++ app/lib/components/product/product-page.tsx
@@ -17,7 +17,37 @@ const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>{
             justify-evenly 
             border-4 border-red-700">
             
-                <div 
+                <Suspense fallback={<ProductFallback/>}>
+                    <ProductInfo listing={listing}/>
+                </Suspense>
+
+            </main>
+
+            <aside className="
+                flex gap-x-5 mt-10 
+                overflow-x-scroll 
+                border-4 border-red-700">
+
+                <Suspense fallback={<Recommendation_Fallback/>}>
+
+                    {similar.map((item)=>
+                        <Recommendation listing={item} key={item.id}/>
+                    )}
+
+                </Suspense>
+
+
+                
+
+            </aside>
+            
+    </>
+    )
+}
+
+const ProductInfo = ({listing}:{listing:Product}) => (
+    <>
+        <div 
                 // dangerous css?
                 className=" 
                 max-w-[200px] 
@@ -85,7 +115,7 @@ const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>{
                 <div className="
                     text-white 
                     text-xl font-light 
-                    w-[300px] pt-10 ">
+                    w-[300px] min-h-[150px] pt-10 ">
                 {
                     listing.description?
                         <div dangerouslySetInnerHTML={{__html:listing.description}}/>
@@ -93,28 +123,86 @@ const ProductPage = ({listing,similar}:{listing:Product,similar:Product[]}) =>{
                         <p>No description found</p>
                 }
                 </div>
-            </main>
-
-            <aside className="
-                flex gap-x-5 mt-10 
-                overflow-x-scroll 
-                border-4 border-red-700">
-
-                <Suspense fallback={<Recommendation_Fallback/>}>
-
-                    {similar.map((item)=>
-                        <Recommendation listing={item} key={item.id}/>
-                    )}
-
-                </Suspense>
-
-
-                
-
-            </aside>
-            
     </>
-    )
-}
+)
+
+const ProductFallback = () => (
+    <>
+        <div 
+            // dangerous css?
+            className=" 
+            max-w-[200px] 
+            min-w-[300px] 
+            flex flex-wrap
+            justify-center
+            ">
+
+
+            
+                    <div className="
+                        bg-white
+                        blur-md 
+                        loading-300
+                        bg-gradient-to-r
+                        from-slate-200  to-transparent
+                        h-[300px] w-[300px]"/>
+                    
+                    
+                     <div className="
+                        flex flex-col 
+                        justify-between flex-wrap">
+                        
+                        <p className="
+                            my-4 min-h-[50px]
+                            text-4xl
+                            text-transparent
+                            bg-clip-text loading
+                            font-light">
+
+                                The product is loading...
+                        </p>
+
+                        <div className="flex flex-col-reverse">
+                            
+                            {/* peer should be before sibling */}
+
+                            <div className="
+                                text-white
+                                loading-300 blur-md
+                                text-2xl
+                                bg-gradient-to-r 
+                                from-red-400 to-yellow-300 
+                                peer-hover:from-red-300 peer-hover:to-yellow-200
+                                bg-clip-text text-transparent">
+
+                                This is a test price
+
+                                <span className="">
+                                    &nbsp;(χωρίς ΦΠΑ)
+                                </span>
+                            
+                            </div>
+                            
+                        </div>
+                    </div>
+
+            
+                        
+                    
+                </div>
+                
+                <div className="
+                    text-white 
+                    text-xl font-light 
+                    w-[300px] pt-10 loading-300 
+                    blur-sm 
+                    bg-gradient-to-r 
+                    from-transparent to-white
+                    bg-clip-text text-transparent ">
+
+                        This is a template description
+                </div>
+    </>
+)
 
 export default ProductPage;
