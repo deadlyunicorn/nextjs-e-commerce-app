26fb2c92ab7997e1370acd0f0c77ab5bddd7a5ed
deadlyunicorn <retroalex1008@gmail.com>
2023-06-28 20:59:04 +0300

    split components into files

---
+++ app/lib/components/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer(){
+    return (
+        <footer 
+            className='
+                min-h-[200px] mt-20 
+                flex justify-center items-center
+                text-white'> 
+            This is a footer
+        </footer>  
+
+    )
+}
---
+++ app/lib/header.tsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+
+export default function Header(){
+
+    return (
+
+        <header>
+
+            <span className="
+                text-4xl
+                bg-gradient-to-b 
+                from-blue-300 to-green-50
+                bg-clip-text text-transparent
+                drop-shadow-[1px_1px_2px_rgba(100,200,100,1)]
+            ">
+                HELLO WORLD AND WELCOME TO MY WEBSTORE
+            </span>
+
+            <Link href={'/'}>Homepage</Link>
+
+            <div className="
+                h-[100px] 
+                items-center justify-center 
+                flex text-white
+            ">
+                Empty box
+            </div>
+
+        </header>
+    )
+}
