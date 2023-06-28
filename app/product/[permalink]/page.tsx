e007b6d36e2e529d8efc0d04f3890e4934cf06fd
deadlyunicorn <retroalex1008@gmail.com>
2023-06-28 04:19:58 +0300

    updated similar search

--- app/product/[permalink]/page.tsx
+++ app/product/[permalink]/page.tsx
@@ -17,77 +17,93 @@ const ItemPage = ({params:{permalink}}:{params:{permalink:string}}) =>{
     
     const similar = items.filter(
 
-        (product)=>(
+        product=>(
             //getting the [slugs] of the current product
+            
             product.categories.map( object => (
                 object.slug
-            )) 
+            ))
+
             //checking if our current listing
             //contains any of the current product
             //slugs - thus making it relevant
             .filter(
-                (category)=>(
+                category=>(
                     categories.includes(category)
                 )
             )
+            .toString()
+            
         )
+
         
-    );
+    )
 
     // console.log(listing);
 
-    return listing ? (
-        <main className="flex flex-wrap border-4 border-white justify-evenly">
-            
-            
-            <div className=" 
-            max-w-[200px]
-            min-w-[300px] 
-            flex flex-wrap
-            justify-center
-            ">
+        console.log(similar.map(item=>item.name));
 
-                <Link href={`/product/${listing.permalink}`}>
-                    <Image src={listing.image?listing.image["url"]:""} 
-                    alt={'aa'}  height={300} width={300}
-                    className="rounded-md h-[300px]" />
-                    
-                    
-                </Link>
-               <div className="flex flex-col justify-between flex-wrap">
-                    <p className="my-4 min-h-[50px]  text-4xl text-white font-light">
-                        {listing.name}
-                    </p>
-                    <div>
-                        <span className="
-                            text-2xl
-                            bg-gradient-to-r from-red-400 to-yellow-300 bg-clip-text text-transparent">
-                            {listing.price["formatted_with_symbol"]}
-                            <span className="text-xs">&nbsp;(χωρίς ΦΠΑ)</span>
-                        </span>
-                        <Button link={listing.checkout_url.checkout}/>
+    return listing ? (
+        <main className=" border-4 border-white ">
+            
+            <div className="flex flex-wrap justify-evenly border-4 border-red-200">
+            
+                <div 
+                // dangerous css?
+                className=" 
+                max-w-[200px] 
+                min-w-[300px] 
+                flex flex-wrap
+                justify-center
+                ">
+
+                    <Link href={`/product/${listing.permalink}`}>
+                        <Image src={listing.image?listing.image["url"]:""} 
+                        alt={'aa'}  height={300} width={300}
+                        className="rounded-md h-[300px]" />
+                        
+                        
+                    </Link>
+                <div className="flex flex-col justify-between flex-wrap">
+                        <p className="my-4 min-h-[50px]  text-4xl text-white font-light">
+                            {listing.name}
+                        </p>
+                        <div>
+                            <span className="
+                                text-2xl
+                                bg-gradient-to-r from-red-400 to-yellow-300 bg-clip-text text-transparent">
+                                {listing.price["formatted_with_symbol"]}
+                                <span className="text-xs">&nbsp;(χωρίς ΦΠΑ)</span>
+                            </span>
+                            <Button link={listing.checkout_url.checkout}/>
+                        </div>
                     </div>
+
+            
+                        
+                    
+                </div>
+                
+                <div className="text-white text-xl font-light max-w-[300px] pt-10">
+                {
+                    listing.description?
+                        <div dangerouslySetInnerHTML={{__html:listing.description}}/>
+                        :
+                        <p>No description found</p>
+                }
                 </div>
 
-          
-                    
+                
                 
             </div>
-            
-            <div className="text-white text-xl font-light max-w-[300px] pt-10">
-            {
-                listing.description?
-                    <p dangerouslySetInnerHTML={{__html:listing.description}}/>
-                    :
-                    <p>No description found</p>
-            }
+            <div>
+                {similar.map(item=>item.name)}
             </div>
-              
-            
 
 
             
 
+            
         </main>
     ):
     (<div>Item not found component</div>)
