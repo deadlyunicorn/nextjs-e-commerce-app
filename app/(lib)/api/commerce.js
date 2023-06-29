af0912ece686153cfc6d888fcec02e6322b80122
deadlyunicorn <retroalex1008@gmail.com>
2023-06-29 22:36:46 +0300

    +api directory +cart

--- app/(lib)/api/commerce.js
+++ app/(lib)/api/commerce.js
@@ -3,5 +3,4 @@ import Commerce from '@chec/commerce.js';
 export const commerce = new Commerce('pk_test_530304959aba3f191ed92e60373ec97da4de0981b5798',true);
 
 
-export const cart = commerce.cart;
-
+export const Cart = commerce.cart;
