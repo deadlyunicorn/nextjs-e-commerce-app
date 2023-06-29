c2c6cf8245c920656cdf73b2cb35754d783654f3
deadlyunicorn <retroalex1008@gmail.com>
2023-06-29 03:13:24 +0300

    +'items' const in component

--- app/product/[permalink]/page.tsx
+++ app/product/[permalink]/page.tsx
@@ -1,7 +1,10 @@
-import { items } from "@/app/lib/commerce";
-import ProductPage from "@/app/lib/components/product/product-page";
+import { commerce } from "@/app/(lib)/commerce";
+import ProductPage from "@/app/(lib)/components/product/product-page";
 
-const ItemPage = ({params:{permalink}}:{params:{permalink:string}}) =>{
+const ItemPage = async({params:{permalink}}:{params:{permalink:string}}) =>{
+
+    const items = await commerce.products.list()
+    .then(result=> result.data);
 
     const listing = items.filter(item=>item.permalink==permalink)[0]
     
