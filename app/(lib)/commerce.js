79a28041f80dac2925ebf500e7a85dd0fb0da452
deadlyunicorn <retroalex1008@gmail.com>
2023-06-29 21:30:24 +0300

    +cart

--- app/(lib)/commerce.js
+++ app/(lib)/commerce.js
@@ -3,5 +3,5 @@ import Commerce from '@chec/commerce.js';
 export const commerce = new Commerce('pk_test_530304959aba3f191ed92e60373ec97da4de0981b5798',true);
 
 
-
+export const cart = commerce.cart;
 
