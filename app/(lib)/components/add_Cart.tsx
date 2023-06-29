edc00dfd7f342a980b045a997ccfb681d958296e
deadlyunicorn <retroalex1008@gmail.com>
2023-06-29 21:27:45 +0300

    button->add_Cart +better button UI

---
+++ app/(lib)/components/add_Cart.tsx
@@ -0,0 +1,87 @@
+'use client'
+
+import { useState } from "react";
+import { cart } from "../commerce";
+import Commerce from "@chec/commerce.js";
+
+export const AddToCart=({item}:{item:string})=>{
+    
+    const handle_Click=()=>{
+        cart.refresh();
+        alert(item +"aaa___" +quantity);
+        say();
+
+    }
+
+    console.log("hello")
+
+    const commerce = new Commerce('pk_test_530304959aba3f191ed92e60373ec97da4de0981b5798'); 
+    
+    const say=async()=>{
+        let time=0;
+        const timer=setInterval(()=>{
+            time++;
+        },1);
+        await commerce.cart.retrieve().then(cart => console.log(cart));
+        console.log("Time was:",time);
+        return timer;
+
+    }
+
+    say();
+
+    const handle_Change=(event: React.FormEvent<HTMLInputElement>)=>{
+        
+        const quantity = Number(event.currentTarget.value);
+        if ( quantity < 11 ){
+            if ( quantity == 0){
+                setQuantity(1);
+            }
+            else{
+                setQuantity(quantity);
+            }
+        }
+    }
+
+    const [quantity,setQuantity]=useState(1);
+
+    return(
+
+        <>
+        <div className="flex gap-x-5">
+            <span className="text-white">
+                Quantity:
+            </span>
+                    
+            <input 
+                type="number"
+                className="
+                    w-10 
+                    rounded-md 
+                    bg-white bg-opacity-[50%]
+                    text-black text-center"
+                max={10}
+                min={1}
+                onChange={handle_Change}
+                value={quantity}
+                name="quantity"
+            />
+
+        </div>
+
+                 {/* peer should be before sibling */}
+     
+        <button 
+            onClick={handle_Click}
+            className="
+                text-black
+                w-full h-10 
+                px-2
+                bg-slate-300 rounded-md ">
+                    ADD TO <b className="font-bold">CART</b>
+        </button>
+        
+        </>
+    )
+
+}  
--- app/(lib)/components/button.tsx
+++
@@ -1,8 +0,0 @@
-export const Button=({link}:{link:string})=>(
-    <button 
-        className="w-full bg-slate-300 h-10 px-2 rounded-md ">
-                BUY 
-    </button>
-
-)
-              
