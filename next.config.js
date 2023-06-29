0999284400d5268c4d9e766c843da69c638f73b4
deadlyunicorn <retroalex1008@gmail.com>
2023-06-29 18:22:49 +0300

    +redirects

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
     images:{
         domains:["cdn.chec.io"]
+    },
+    async redirects(){
+        return [
+            {
+                source: '/explore',
+                destination: '/explore/1',
+                permanent: true
+             },
+             {
+                source: '/categories',
+                destination: '/#categories',
+                permanent: true
+             }
+         ]
     }
 }
 
