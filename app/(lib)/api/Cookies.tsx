a918f6a5020de3e8e117646175188055b2276de2
deadlyunicorn <retroalex1008@gmail.com>
2023-07-01 00:15:40 +0300

    save cart to a cookie with a form

--- app/(lib)/api/Cookies.tsx
+++ app/(lib)/api/Cookies.tsx
@@ -3,31 +3,40 @@ import { getCart } from "./Cart";
 
 export default async function CookiesElement(){
 
-    const cookieStore = cookies();
+    const cookieStore:any = cookies();
 
 
-    const setCookie = async(cart_id:any) =>{
+    const setCookie = async(form:any) =>{
     'use server'
 
 
     // cookieStore.set('cart_id',cart_id);
-    cookies().set('test_cookie',`${JSON.stringify(cart_id)}`);
+    cookies().set('test_cookie',`${form.get('cookieName')}`);
 
 }
-const cart = await getCart();
-console.log(cart.id);
+
+    const getFormData = async(form:any) => {
+        'use server'
+        console.log(form.get('cookieName'));
+    }
+    const cart = await getCart();
+    console.log(cart.id);
 
     return(
-        <div>
-            Cookie info:{JSON.stringify(
-                cookieStore,
-            )}
+    <div className="text-green-500">
+        Cookie info:{
+            cookieStore["_headers"]["headers"]?
+            cookieStore["_headers"]["headers"]["cookie"]
+            :"loading"
+        }
 
-            <form action={setCookie}>
-                <button type="submit" value={cart.id}>
-                    setCookie
-                </button>
-            </form>
-        </div>
-    )
+        <form action={setCookie}>
+            <button type="submit">
+                setCookie
+            </button>
+            <input name="cookieName" value={cart.id}>
+            </input>
+        </form>
+    </div>
+)
 }
