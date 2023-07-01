6cabac758995bff744d7f5e6b8c816cbaf20d2db
deadlyunicorn <retroalex1008@gmail.com>
2023-07-01 03:29:01 +0300

    + (ctrl+shift+i) (reformat)

--- app/(lib)/api/items.ts
+++ app/(lib)/api/items.ts
@@ -1,32 +1,34 @@
 import { ProductCollection } from "@chec/commerce.js/features/products";
 
-export const fetchItems = async(params:{[key:string]:string}):Promise<ProductCollection["data"]> => {
-  
-    
-    const url = new URL(
-      "https://api.chec.io/v1/products"
-    )
+export const fetchItems = async (params: { [key: string]: string }): Promise<ProductCollection["data"]> => {
 
-  
-    Object.keys(params)
-      .forEach(key => url.searchParams //appending to url
-        .append(
-          key,params[key]
+
+  const url = new URL(
+    "https://api.chec.io/v1/products"
+  )
+
+
+  Object.keys(params)
+    .forEach(key => url.searchParams //appending to url
+      .append(
+        key, params[key]
       ))
-  
-    const headers = {
-        "X-Authorization": `${process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}`,
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-    };
-  
-    const res=await fetch(url, {
-        method: "GET",
-        headers: headers,
-    })
-  
-    if (!res.ok){
-        throw new Error(`Fetch failed`);
-    }
-    return res.json().then(result=>result.data);
+
+  const headers = {
+    "X-Authorization": `${process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}`,
+    "Accept": "application/json",
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*" //stop fetch errors
+
+  };
+
+  const res = await fetch(url, {
+    method: "GET",
+    headers: headers,
+  })
+
+  if (!res.ok) {
+    throw new Error(`Fetch failed`);
   }
+  return res.json().then(result => result.data);
+}
