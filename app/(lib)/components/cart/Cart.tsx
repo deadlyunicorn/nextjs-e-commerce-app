5dddefd2f65207a107ed5449832593d8f6ac2063
deadlyunicorn <retroalex1008@gmail.com>
2023-07-01 03:29:37 +0300

    clean up your mind (folders)

---
+++ app/(lib)/components/cart/Cart.tsx
@@ -0,0 +1,20 @@
+
+
+const CartComponent = async () => {
+
+    // const cart = await getCart();
+
+    return (
+        <>
+            <div className="text-white">
+                {/* Your cart is {JSON.stringify(cart.id)} */}
+            </div>
+        </>
+    )
+}
+
+
+
+
+
+export default CartComponent;
