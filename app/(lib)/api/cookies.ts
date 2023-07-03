3622d277ee9b864543efd10e55c82266ab174111
deadlyunicorn <retroalex1008@gmail.com>
2023-07-04 01:29:28 +0300

    moved file location

---
+++ app/(lib)/api/cookies.ts
@@ -0,0 +1,36 @@
+"use server"
+import { cookies } from "next/headers";
+
+
+    export const setCookie = async(cart_id: string)=>{
+
+        'use server'
+        cookies().set(
+        { 
+            name:'cart_id',
+            value: cart_id,
+            maxAge: 60 * 60 * 24 * 30,
+            path: "/" 
+        });
+    
+    }
+
+    export const getCookies= ()=>{
+        //Not async but requires await?!?! idk
+        //Don't mark it as async however...
+        return cookies().getAll();
+    }
+
+    export const getCartCookie= () => {
+        return cookies().get('cart_id')?.value;
+    }
+
+    export const deleteCookie= async (cookie_name: string) => {
+        'use server'
+        cookies().set({
+            name: cookie_name,
+            value: "",
+            maxAge: 0,
+            path: "/"
+        })
+    }
