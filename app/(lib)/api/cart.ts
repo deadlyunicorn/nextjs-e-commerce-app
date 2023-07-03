e417fd30e415b02fabc663b51d07058f79514079
deadlyunicorn <retroalex1008@gmail.com>
2023-07-04 01:48:46 +0300

    caching the cart?

--- app/(lib)/api/cart.ts
+++ app/(lib)/api/cart.ts
@@ -1,9 +1,10 @@
 'use server'
 
 import { Cart } from "@chec/commerce.js/types/cart";
+import { getCartCookie } from "./cookies";
 
 
-export const getCart = async () : Promise<Cart> => {
+export const createCart = async () : Promise<Cart> => {
     const url = new URL(
         "https://api.chec.io/v1/carts"
     )
@@ -25,4 +26,31 @@ export const getCart = async () : Promise<Cart> => {
     }
 
     return res.json();
+}
+
+export const getCart = async () =>{
+    const url = new URL(
+        `https://api.chec.io/v1/carts/${await getCartCookie()}`
+    )
+
+    const headers = {
+        "X-Authorization": `${process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}`,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    };
+
+    const res = await fetch(url, {
+        method: "GET",
+        headers: headers,
+        next: {revalidate: 60 * 60 * 24 * 10}
+
+    })
+
+    if (!res.ok) {
+        throw new Error(`Fetch failed`);
+    }
+
+    return res.json();
+
 }
