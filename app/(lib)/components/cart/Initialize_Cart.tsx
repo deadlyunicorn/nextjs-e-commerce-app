63d2bf16cbe2b7c8fe319eb1ff9bb637540cfa96
deadlyunicorn <retroalex1008@gmail.com>
2023-07-04 01:49:22 +0300

    refactoring and identifier updates

--- app/(lib)/components/cart/Initialize_Cart.tsx
+++ app/(lib)/components/cart/Initialize_Cart.tsx
@@ -1,28 +1,19 @@
 'use client'
 
 
-import { useEffect, useState, useTransition } from "react"
-import {  deleteCookie, getCartCookie, getCookies, setCookie } from "../../api/cookies"
-import { getCart } from "../../api/cart";
+import { useEffect  } from "react"
+import { getCookies, setCookie } from "../../api/cookies"
+import { createCart, getCart } from "../../api/cart";
 
 //No async() on client components..
 export const CookieVerify = () => {
 
-    const [cart,setCart] = useState<undefined|string> (undefined);
-
-    //need this to set the cookie.
+    
     //check those:
-
     //https://nextjs.org/docs/app/api-reference/functions/cookies#cookiessetname-value-options
     //https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions#custom-invocation-using-starttransition
 
 
-    //actually I commented out the code and it works without it ??????? WTF.
-
-      // startTransition(async()=>{
-          
-        //     setCookie();
-        // })
 
     useEffect(()=>{
 
@@ -32,7 +23,6 @@ export const CookieVerify = () => {
             const cartCookieArray = [...await getCookies()]
             .filter(cookie=>{
                 if(cookie.name=="cart_id"){
-                    setCart(cookie.value);
                     return true;
                 }})
 
@@ -41,7 +31,7 @@ export const CookieVerify = () => {
 
             if (cartCookieArray.length == 0){
                 
-                await getCart().then(
+                await createCart().then(
                     async(cart)=>{
                         await setCookie(cart.id)
                     }
@@ -52,12 +42,12 @@ export const CookieVerify = () => {
 
         })()
 
-    },[cart])
+    },[])
+
 
     return(
           <>
-          Your cart is:
-            {cart}
+            
           </>
     )
 }
