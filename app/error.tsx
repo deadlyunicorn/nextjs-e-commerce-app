225559a525a8edcebe4819b1ca134b933f0af3a0
deadlyunicorn <retroalex1008@gmail.com>
2023-07-04 05:28:39 +0300

    Link -> useRouter() (Not sure if it works better on error pages)

--- app/error.tsx
+++ app/error.tsx
@@ -1,15 +1,19 @@
 'use client'
 import Link from "next/link";
+import {  useRouter } from "next/navigation";
 
 export default function Error () {
+    const router = useRouter();
     return(
         <>
             <h1>
                 An error has occured, page may not exist.
             </h1>
-            <Link href='/'>
+            <button onClick={()=>{
+                router.replace("/");
+            }}>
                 Reload
-            </Link>
+            </button>
         </>
     )
 }
