a2f7c9703ce72546890edb7939f0f673bf669759
deadlyunicorn <retroalex1008@gmail.com>
2023-07-04 05:54:59 +0300

    forceRetry -> useRouter.refresh()

--- app/(lib)/components/cart/Initialize_Cart.tsx
+++ app/(lib)/components/cart/Initialize_Cart.tsx
@@ -1,18 +1,20 @@
 'use client'
-import { useEffect, useState  } from "react"
+import { useEffect  } from "react"
 import { getCookies, setCookie } from "../../api/cookies"
 import { createCart, getCart } from "../../api/cart";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import { setCart } from "../redux/cartSlice";
+import { useRouter } from "next/navigation";
 
 
 //No async() on client components..!!!
 export const CookieVerify = () => {
 
-    const [retry,forceRetry] = useState(0);
     const cart = useSelector((state:RootState)=> state.cart.value);
     const dispatch = useDispatch();
+    const router = useRouter();
+
     
     //check those:
     //https://nextjs.org/docs/app/api-reference/functions/cookies#cookiessetname-value-options
@@ -38,7 +40,10 @@ export const CookieVerify = () => {
                 await createCart().then(
                     async(cart)=>{
                         await setCookie(cart.id)
-                        forceRetry(retry+1);
+                        .then(()=>{
+                            router.refresh();
+                        })
+                        
                     }
                 )
                 
@@ -50,7 +55,7 @@ export const CookieVerify = () => {
 
         })()
 
-    },[retry,cart,dispatch])
+    },[cart,dispatch,router])
 
 
     return(
