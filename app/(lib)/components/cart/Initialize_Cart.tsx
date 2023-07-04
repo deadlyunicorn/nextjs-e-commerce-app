055a9a18ab45d65460b840e95eaa86b928ea75b9
deadlyunicorn <retroalex1008@gmail.com>
2023-07-04 03:50:24 +0300

    +cartProvider

--- app/(lib)/components/cart/Initialize_Cart.tsx
+++ app/(lib)/components/cart/Initialize_Cart.tsx
@@ -1,13 +1,18 @@
 'use client'
-
-
-import { useEffect  } from "react"
+import { useEffect, useState  } from "react"
 import { getCookies, setCookie } from "../../api/cookies"
 import { createCart, getCart } from "../../api/cart";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "../redux/store";
+import { setCart } from "../redux/cartSlice";
 
-//No async() on client components..
+
+//No async() on client components..!!!
 export const CookieVerify = () => {
 
+    const [retry,forceRetry] = useState(0);
+    const cart = useSelector((state:RootState)=> state.cart.value);
+    const dispatch = useDispatch();
     
     //check those:
     //https://nextjs.org/docs/app/api-reference/functions/cookies#cookiessetname-value-options
@@ -21,10 +26,9 @@ export const CookieVerify = () => {
 
 
             const cartCookieArray = [...await getCookies()]
-            .filter(cookie=>{
-                if(cookie.name=="cart_id"){
-                    return true;
-                }})
+            .filter(cookie=>(
+                cookie.name=="cart_id")
+            )
 
             //get the value => cartCookieArray[0].value
             // await deleteCookie("cart_id");
@@ -34,20 +38,28 @@ export const CookieVerify = () => {
                 await createCart().then(
                     async(cart)=>{
                         await setCookie(cart.id)
+                        forceRetry(retry+1);
                     }
                 )
                 
+            }
+            else{
+                dispatch(setCart( cartCookieArray[0].value ) )
             }
             
 
         })()
 
-    },[])
+    },[retry,cart,dispatch])
 
 
     return(
           <>
-            
+            {cart}
           </>
     )
 }
+
+
+
+
