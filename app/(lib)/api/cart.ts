3dfb3f06ce835a56778762166ed0bc6a67d5b42b
deadlyunicorn <retroalex1008@gmail.com>
2023-07-04 04:45:16 +0300

    +addToCart

--- app/(lib)/api/cart.ts
+++ app/(lib)/api/cart.ts
@@ -53,3 +53,39 @@ export const getCart = async () =>{
     return res.json();
 
 }
+
+export const addCart = async (id:string,quantity:string) =>{
+    "use server"
+    const url = new URL(
+        `https://api.chec.io/v1/carts/${await getCartCookie()}`
+    )
+
+    const headers = {
+        "X-Authorization": `${process.env.NEXT_PUBLIC_CHEC_PUBLIC_API_KEY}`,
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*"
+    };
+
+    const body = {
+        id: id,
+        quantity: quantity,
+    }
+
+    const res = await fetch(url, {
+        method: "POST",
+        headers: headers,
+        body: JSON.stringify(body)
+    })
+
+    console.log(JSON.stringify(body))
+
+    if (!res.ok) {
+        throw new Error(`Fetch failed ${res.status}`);
+    }
+
+    return res.json();
+
+}
+
+
