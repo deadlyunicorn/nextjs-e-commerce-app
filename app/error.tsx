c0bc365de8b7972aa81c7fa399adf8b7e34e0030
deadlyunicorn <retroalex1008@gmail.com>
2023-07-06 23:05:28 +0300

    Better Error page UX

--- app/error.tsx
+++ app/error.tsx
@@ -2,23 +2,51 @@
 import Link from "next/link";
 import {  useRouter } from "next/navigation";
 
-export default function Error () {
+interface ErrorType{
+    [key:string]:string;
+
+}
+
+export default function Error ({error}:{error:ErrorType}) {
     const router = useRouter();
     return(
-        <>
-            <h1>
-                An error has occured, page may not exist.
-            </h1>
-            <button onClick={()=>{
-                router.replace("/");
-                //we may also need to trigger a refresh..
-                //but not on the current path.
-                //Link might be okay?
-                //idk i think i had an error..
+            <div className="
+                
+                mb-96
+                rounded-md py-2 text-lg
+                text-white bg-white
+                [&>*]:my-4
+                 ">
 
-            }}>
-                Reload
-            </button>
-        </>
+                <div>
+                    <span 
+                    className="
+                        text-center bg-red-500 
+                        px-2 py-1 rounded-md">
+                        An error has occured: {JSON.stringify(error)}
+                    </span>
+                    
+                </div>
+                <p className="text-black">
+                    Page may not exist.
+                </p>
+
+                <button 
+                
+                className="
+                mt-2
+                bg-blue-600 rounded-md px-1"
+                onClick={()=>{
+                    router.replace("/");
+                    // router.refresh();
+                    //we may also need to trigger a refresh..
+                    //but not on the current path.
+                    //Link might be okay?
+                    //idk i think i had an error..
+
+                }}>
+                    Reload
+                </button>
+            </div>
     )
 }
