fd4d9100cf0e82a46ce4f4bf6c88a8bdbf09bb3f
deadlyunicorn <retroalex1008@gmail.com>
2023-07-07 04:27:41 +0300

    some minor discarded - unused changes..

--- app/(lib)/components/redux/cartSlice.ts
+++ app/(lib)/components/redux/cartSlice.ts
@@ -1,6 +1,7 @@
+import { Cart } from "@chec/commerce.js/types/cart";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
-const initialState:{ value: undefined|string} = {
+const initialState:{ value: undefined|Cart} = {
     value : undefined
 }
 
@@ -8,7 +9,7 @@ export const cartSlice = createSlice({
     name: 'cart',
     initialState,
     reducers: {
-        setCart : (state, action:PayloadAction<string>) =>{
+        setCart : (state, action:PayloadAction<Cart>) =>{
             state.value = action.payload
         }
     }
