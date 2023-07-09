1ea0fcc9f5ed96dd94ff914a37f3b5fcc1d78001
deadlyunicorn <retroalex1008@gmail.com>
2023-07-09 03:16:23 +0300

    fix: no layout shifts when temp  cart_id component is loading

--- app/(lib)/components/cart/Initialize_Cart.tsx
+++ app/(lib)/components/cart/Initialize_Cart.tsx
@@ -60,7 +60,9 @@ export const CookieVerify = () => {
 
     return(
           <>
-            {cart_id}
+            <div className="absolute text-red-700 font-bold left-[70vw] top-[15vh]">
+                {cart_id || "loading cart..."}
+            </div>
           </>
     )
 }
