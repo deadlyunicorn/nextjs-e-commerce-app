fd7285d43c23c1ad4dc3e701185ab32f7566d2dc
deadlyunicorn <retroalex1008@gmail.com>
2023-07-10 17:26:25 +0300

    +SVG cart

--- app/(lib)/components/cart/Cart.tsx
+++ app/(lib)/components/cart/Cart.tsx
@@ -1,7 +1,7 @@
 import { Cart } from "@chec/commerce.js/types/cart";
 import { getCart } from "../../api/cart";
 import Link from "next/link";
-
+import Image from "next/image";
 
 const CartComponent = async () => {
 
@@ -10,9 +10,33 @@ const CartComponent = async () => {
     return (
         <>
             <div className="text-white">
-                <Link href="/cart">
-                    You have {cart?.total_items||0} items in cart.
+                <Link href="/cart" className="group">
+                    <figure className="flex items-center gap-x-1">
+                        
+                        <svg 
+                            viewBox="0 0 18 18"
+                            className="
+                            fill-none 
+                            stroke-slate-300 group-hover:stroke-slate-50 peer 
+                            w-8 h-8
+                            aspect-square ">
+
+                            <path
+                                d="M 5.551206,9.9204978 5.6146856,3.6976653 11.551206,0.92049781 11.407884,9.3296041"/>
+                            <path
+                                d="M 1.551206,9.9204978 3.6374886,15.611381 H 13.168462 L 15.551206,8.9204978 Z"/>
+                    
+                        </svg>
+                        
+                        <figcaption className="text-slate-300 group-hover:text-slate-50">
+                            {cart?.total_items||0} item(s)
+                        </figcaption>
+                    </figure>
+
+
+
                 </Link>
+
                 {/* Added: show 0 when cart hasn't loaded yet */}
             </div>
         </>
