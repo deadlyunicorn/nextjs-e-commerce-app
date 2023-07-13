d4fa7fe16574215090072f7b8d50ca31967683e7
deadlyunicorn <retroalex1008@gmail.com>
2023-07-13 21:09:23 +0300

    + google provider +signIn callback options

--- app/(Admin)/api/auth/[...nextauth]/route.ts
+++ app/(Admin)/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,36 @@
 import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google"
 
 
 const handler = NextAuth({
     providers:[
         GitHubProvider({
             clientId: process.env.GITHUB_ID!,
-            clientSecret: process.env.GITHUB_SECRET!
+            clientSecret: process.env.GITHUB_SECRET!,
+        }),
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
+        
+        
     ],
     callbacks:{
         async redirect({ url,baseUrl }){
            return '/admin'
+        },
+        async signIn({account,profile}){
+            if (account?.provider === "google"){
+
+                return profile?.email==process.env.ALLOWED_EMAIL;
+
+                // return profile?.email_verified && profile.email!.endsWith("@gmail.com") 
+            }
+            return profile?.email==process.env.ALLOWED_EMAIL;
+
         }
-    }
+    },
 
 })
 
