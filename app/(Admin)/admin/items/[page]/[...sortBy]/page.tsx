332188653f28d8dd0493b7c2dc2b53228df4ef90
deadlyunicorn <retroalex1008@gmail.com>
2023-07-20 06:37:41 +0300

    +Create new item option +UI rearrangement +handling errors

--- app/(Admin)/admin/items/[page]/[...sortBy]/page.tsx
+++ app/(Admin)/admin/items/[page]/[...sortBy]/page.tsx
@@ -36,6 +36,10 @@ const ItemsList = async({params}:{params:{page:string,sortBy:string[]}}) => {
         page: String(page),
         sortDirection: sortingOrder //or desc
     });
+    if (!items || items.length<1){
+        redirect('/admin')
+    }
+
     const nextPage= await fetchItemsADMIN({
         limit:String(limit),
         sortBy: sortBy,
@@ -59,108 +63,125 @@ const ItemsList = async({params}:{params:{page:string,sortBy:string[]}}) => {
             justify-start">
 
             
-            <div aria-label="page number">Page Number: {page}</div>
+                
 
-            <ul
-                className="w-full">
+                <ul
+                    className="w-full">
 
-                <li 
-                    className="
-                        py-4
-                        grid-cols-4
-                        grid 
-                        border-slate-200
-                        dark:border-slate-800
-                        border
-                        w-full
-                        justify-items-center
-                        font-semibold"
-                key="Table titles">
-                    <div>Image</div> 
-                    <div>Name</div> 
-                    <div>Price</div> 
-                    <div className="">Stock</div> 
-                </li>
-                {
-                items.map(item=>(
                     <li 
                         className="
-                            group h-[80px]
-                            xs:h-[50px]
-                            relative
-                            "
-                        key={item.id}>
-                        <div className="
-                            grid-cols-4 justify-items-center place-items-center
-                            border border-slate-200 dark:border-slate-800
-                            grid  w-full h-full overflow-hidden
-                            "> 
-                            <Image 
-                                className="aspect-square"
-                                alt={"Product listing"}
-                                width={50}
-                                height={50}
-                                src={item.image?.url || "/image.png"}/>
-                            
-                            <div>{item.name}</div>
-                            <div>{item.price.raw}</div>
-                            <div className="justify-self-center">{item.inventory.managed? item.inventory.available:"Not managed"}</div>
-
-                        </div>
-                        
-                        <Link 
-                        
-                        href={`/admin/edit/${item.id}`}
-                        className="
-                            backdrop-blur-sm
-                            bg-slate-200 bg-opacity-20
-                            dark:bg-slate-800 dark:bg-opacity-20
-                            w-full h-full
-                            group-hover:justify-center group-hover:items-center
-                            hidden group-hover:flex absolute bottom-0 ">
-                            Press to edit
-                        </Link>
+                            py-4
+                            grid-cols-4
+                            grid 
+                            border-slate-200
+                            dark:border-slate-800
+                            border
+                            w-full
+                            justify-items-center
+                            font-semibold"
+                    key="Table titles">
+                        <div>Image</div> 
+                        <div>Name</div> 
+                        <div>Price</div> 
+                        <div className="">Stock</div> 
                     </li>
-                ))
-                }
+                    {
+                    items.map(item=>(
+                        <li 
+                            className="
+                                group h-[80px]
+                                xs:h-[50px]
+                                relative
+                                "
+                            key={item.id}>
+                            <div className="
+                                grid-cols-4 justify-items-center place-items-center
+                                border border-slate-200 dark:border-slate-800
+                                grid  w-full h-full overflow-hidden
+                                "> 
+                                <Image 
+                                    className="aspect-square"
+                                    alt={"Product listing"}
+                                    width={50}
+                                    height={50}
+                                    src={item.image?.url || "/image.png"}/>
+                                
+                                <div>{item.name}</div>
+                                <div>{item.price.raw}</div>
+                                <div className="justify-self-center">{item.inventory.managed? item.inventory.available:"Not managed"}</div>
 
-                <li 
-                    className="
-                        py-4 
-                        grid grid-cols-2
-                        border
-                        border-slate-200
-                        dark:border-slate-800
-                        w-full 
-                        justify-items-center">
-
-                    {page>1
-                    ?
-                    <Link href={`/admin/items/${page-1}/${sortBy}/${sortingOrder||""}`}
-                    className="hover:underline">
-                        Previous Page
-                    </Link>
-                    :<div></div>
+                            </div>
+                            
+                            <Link 
+                            
+                            href={`/admin/items/edit/${item.id}`}
+                            className="
+                                backdrop-blur-sm
+                                bg-slate-200 bg-opacity-20
+                                dark:bg-slate-800 dark:bg-opacity-20
+                                w-full h-full
+                                group-hover:justify-center group-hover:items-center
+                                hidden group-hover:flex absolute bottom-0 ">
+                                Press to edit
+                            </Link>
+                        </li>
+                    ))
                     }
-                    {nextPage?
-                    <Link href={`/admin/items/${page+1}/${sortBy}/${sortingOrder||""}`}
-                    className="hover:underline">
-                        Next Page
-                    </Link>
-                    :<div></div>
+
+                    <li 
+                        className="
+                            py-4 
+                            grid grid-cols-2
+                            border
+                            border-slate-200
+                            dark:border-slate-800
+                            w-full 
+                            justify-items-center">
+
+                        {page>1
+                        ?
+                        <Link href={`/admin/items/${page-1}/${sortBy}/${sortingOrder||""}`}
+                        className="hover:underline">
+                            Previous Page
+                        </Link>
+                        :<div></div>
+                        }
+                        {nextPage?
+                        <Link href={`/admin/items/${page+1}/${sortBy}/${sortingOrder||""}`}
+                        className="hover:underline">
+                            Next Page
+                        </Link>
+                        :<div></div>
+                        }
+                        
+                    </li>
+                </ul>
+
+                <div className="flex justify-between w-full">
+
+                    <div className="flex flex-col gap-y-4">
+                        <SortBy currentSort={sortBy}/>
+                        {sortingOrder
+                        ?<Link className="hover:underline" href={'./'}>Reverse Order</Link>
+                        :<Link className="hover:underline" href={`${sortBy}/desc`}>Reverse Order</Link>
                     }
-                    
-                </li>
-            </ul>
 
-            <div className="flex justify-between w-full">
-                <SortBy currentSort={sortBy}/>
-                {sortingOrder
-                ?<Link href={'./'}>Reverse Order</Link>
-                :<Link href={`${sortBy}/desc`}>Reverse Order</Link>
-                }
+                    </div>
 
-            </div>
+                    <div className="flex flex-col gap-y-4">
+                        <div aria-label="page number">Page Number: {page}</div>
+                        <Link 
+                            href="/admin/items/create/new"
+                            className="
+                                hover:underline
+                                text-green-400
+                                dark:text-green-600
+                                capitalize font-bold">
+                                Create New item
+                        </Link>
+                    </div>
+
+                </div>
 
             </div>
 
