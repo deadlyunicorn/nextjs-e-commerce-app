f573be6d7c1ea18389eb1e698d4beb835f5a6dd3
deadlyunicorn <retroalex1008@gmail.com>
2023-07-24 18:19:28 +0300

    +Unauthorized page

---
+++ app/(Admin)/admin/unauthorized/page.tsx
@@ -0,0 +1,72 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
+export default function Unauthorized() {
+    
+    const router = useRouter();
+    return(
+            <div className="
+                
+                mb-96
+                rounded-md py-2 text-lg
+                dark:bg-slate-950
+                bg-slate-200
+                [&>*]:my-4 
+                w-full
+
+                mt-[5vh] 
+                xl:max-w-4xl 
+                md:max-w-3xl
+                text-center xs:px-5
+                max-w-[100%]
+
+                flex flex-col flex-wrap
+                items-center
+                ">
+
+                <div>
+                    <span 
+                    className="
+                        text-center bg-red-500 text-white 
+                        px-2 py-1 rounded-md">
+                            Unauthorized
+                    </span>
+                    
+                </div>
+
+                <div className="flex gap-x-4 text-white">
+
+                <button className="w-fit
+                    mt-2
+                    hover:bg-blue-500
+                    bg-blue-600 rounded-md px-1"
+                    onClick={()=>{
+                        
+                        // @ts-ignore
+                        router.back()
+                    }}>
+                        Back
+                    </button>
+
+                    
+
+                    <button className="w-fit
+                    mt-2
+                    hover:bg-blue-500
+                    bg-blue-600 rounded-md px-1"
+                    onClick={()=>{
+                        
+                        // @ts-ignore
+                        router.replace("/admin")
+                    }}>
+                        Homepage
+                    </button>
+
+                   
+
+                </div>
+
+            </div>
+    )
+}
