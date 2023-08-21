e59e72779a72256883563db23d97a326334aa8fb
deadlyunicorn <retroalex1008@gmail.com>
2023-08-21 04:44:05 +0300

    +async getOrder()

--- app/(Admin)/admin/orders/api/fetchOrders.ts
+++ app/(Admin)/admin/orders/api/fetchOrders.ts
@@ -33,4 +33,27 @@ export const getOrders = async (params: { [key: string]: string }): Promise<Orde
     }
     return res.json().then(result => result.data);
 }
-  
+
+export const getOrder = async(order_id:string):Promise<Order>=>{
+  const url = new URL(
+    `https://api.chec.io/v1/orders/${order_id}`
+);
+
+const headers = {
+  "X-Authorization": `${process.env.PRIVATE_API_KEY}`,
+  "Accept": "application/json",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*" //stop fetch errors
+
+};
+const res = await fetch(url, {
+  method: "GET",
+  headers: headers,
+  next: {revalidate: 0}
+})
+
+if (!res.ok) {
+  throw new Error(`Fetch failed - (${res.status}) ${res.statusText}`);
+}
+return res.json().then(result => result);
+}
