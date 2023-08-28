996d0340b7b60098354679201bc75e9fb7788cdb
deadlyunicorn <retroalex1008@gmail.com>
2023-08-28 17:03:30 +0300

    css adjustments (CoolButton)

--- app/(User)/checkout/[...cart_id]/SubmitButton.tsx
+++ app/(User)/checkout/[...cart_id]/SubmitButton.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { useState } from "react"
 import LoadingScreen from "../../loader/page";
+import { CoolButton } from "@/app/(Shared)/components/Global";
 
 const SubmitButton = ({fail}:{fail:boolean}) => {
 
@@ -23,34 +24,29 @@ const SubmitButton = ({fail}:{fail:boolean}) => {
         </div>
         }
         
-        <input 
-        type="submit"
-        onClick={()=>{setLoading(true)}}
-        className="
-            hover:underline 
-            disabled:no-underline
-            disabled:cursor-default
-            text-lg 
-            cursor-pointer 
-            
-            dark:bg-slate-950
-            disabled:dark:bg-slate-950
-            dark:hover:bg-slate-900
-
-            bg-slate-200
-            disabled:bg-slate-200
-            hover:bg-slate-100
-
-            rounded-md px-2 py-1
-            w-fit "
-            value={loading?"Loading...":(fail?"Try Again":"Submit")}
+        <CoolButton>
+            <input 
+            type="submit"
+            onClick={()=>{setLoading(true)}}
+            className="
+                hover:underline 
+                disabled:no-underline
+                disabled:cursor-default
+                text-lg 
+                cursor-pointer 
+                
+                rounded-md px-2 py-1
+                w-fit "
+                value={loading?"Loading...":(fail?"Try Again":"Submit")}
             />
+        </CoolButton>
         </>
 
     )
 }
 
 export const MockSubmitButton=({fail}:{fail:boolean}) => (
+    <CoolButton>
     <input 
         type="submit"
         className="
@@ -60,20 +56,13 @@ export const MockSubmitButton=({fail}:{fail:boolean}) => (
             text-lg 
             cursor-pointer 
             
-            dark:bg-slate-950
-            disabled:dark:bg-slate-950
-            dark:hover:bg-slate-900
-
-            bg-slate-200
-            disabled:bg-slate-200
-            hover:bg-slate-100
-
             rounded-md px-2 py-1
             w-fit "
 
             value={fail?"Try Again":"Submit"}
 
         />
+        </CoolButton>
 )
 
 export default SubmitButton;
