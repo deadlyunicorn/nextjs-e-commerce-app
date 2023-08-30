42afcc90e3fda143b14ec871be2bce3da5fd8163
deadlyunicorn <retroalex1008@gmail.com>
2023-08-30 05:01:21 +0300

    +display user favorites and orders

--- app/(User)/myProfile/page.tsx
+++ app/(User)/myProfile/page.tsx
@@ -4,6 +4,12 @@ import { Order } from "@chec/commerce.js/types/order";
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
+import { getFavorites } from "../(lib)/api/favorites";
+import { fetchItems } from "../(lib)/api/items";
+import { Product } from "@chec/commerce.js/types/product";
+import Image from "next/image";
+import Link from "next/link";
+import { ListOrders, getDate } from "@/app/(Admin)/admin/orders/[page]/[...sortBy]/page";
 
 const UserPage = async()=>{
 
@@ -14,12 +20,30 @@ const UserPage = async()=>{
         redirect('/signin');
     }
 
+    const email = session.user?.email;
+
     const userOrders:Order[] = await getOrders({
         limit:String(10),
         page: String(1),
-        query: session.user?.email+""
+        query: email+""
       });
 
+    
+    const favorites = email
+    ?await getFavorites(email)
+        .then(async(favorites)=>{
+            if (favorites&&favorites.length>0){
+                return await fetchItems(
+                    // @ts-ignore
+                    {query:favorites}
+                )
+            }
+        }
+
+        )
+        .catch(e=>[])
+        :null;
+    
     
 
     return (
@@ -49,6 +73,49 @@ const UserPage = async()=>{
             <UserSection>
                 <h1 className="text-2xl">Favorites</h1>
 
+                {
+                (favorites&&favorites.length>0)
+                ?
+                <ul className="
+                    py-4 px-2
+                    gap-y-2
+                    justify-items-center
+                    place-items-center
+
+                    grid-cols-2
+                    xs:grid-cols-3
+                    sm:grid-cols-4
+                    xl:grid-cols-5
+                    grid ">
+                    {favorites.map(
+                        item=>
+                        <li 
+                            key={item.id}>
+
+                            <Link 
+                            href={`/product/${item.permalink}`}
+                            className="
+                            hover:underline
+                            hover:brightness-110
+                            hover:saturate-150
+                            gap-y-2 my-2
+                            flex flex-col
+                            items-center
+                            w-full h-full">
+                            <Image
+                                
+                                alt={`Image of ${item.name}`}
+                                className="w-20 h-20 bg-white rounded-md"
+                                width={80} height={80} 
+                                src={item.image?.url||"/image.png"}/>
+                            {item.name}
+                            </Link>
+                        </li>)}
+                </ul>
+                
+                :<p>You don&quot;t have any favorites yet.</p>}
+
+
 
             </UserSection>
 
@@ -56,7 +123,10 @@ const UserPage = async()=>{
 
                 <h1 className="text-2xl">Orders</h1>
                 {(userOrders&&userOrders.length>0)
-                ?<p>Great there are orders!</p>
+                ?
+                <ul>
+                    {ListOrders(userOrders)}
+                </ul>
                 :<p>You haven&apos;t made any orders yet!</p>
                 }
 
